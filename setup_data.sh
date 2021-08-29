aadc7bbd58edec59f58ef31b936ae4ea868048d9
Lý Anh Thy <89233480+ThyLy02@users.noreply.github.com>
2021-08-29 18:15:08 +0700

    Update setup_data.sh

--- setup_data.sh
+++ setup_data.sh
@@ -1,7 +1,6 @@
 mkdir data
 python ./src/get_data.py
 <<<<<<< HEAD
-unzip '..\data\brain-tumor-dataset.zip' -d ./data
+extract '..\data\brain-tumor-dataset.zip' -d ./data
 =======
-unzip 'F:\data\brain-tumor-dataset.zip' -d ./data
 >>>>>>> 4f736d529da517330be0e43fa431f2b1b80a60f2
